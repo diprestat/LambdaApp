59f288c1a8d061179f8824542812f35a3e210ff0
Prestat Dimitri <di.prestat@gmail.com>
2021-02-26 05:21:18 +0100

    Add fault in LockManager for Code smell detection testing.

--- app/src/main/java/com/core/lambdaapp/LockManager.java
+++ app/src/main/java/com/core/lambdaapp/LockManager.java
@@ -13,14 +13,20 @@ public class LockManager {
     public LockManager(PowerManager powerManager) {
         this.powerManager = powerManager;
         this.completeWakeLock = this.powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "complete:");
+        System.out.println("Wakelock 1 : " + System.identityHashCode(this.completeWakeLock));
         this.incompleteWakeLock = this.powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "incomplete:");
+        System.out.println("Wakelock 2 : " + System.identityHashCode(this.completeWakeLock));
         this.missingWakeLock = this.powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "missing:");
+        System.out.println("Wakelock 3 : " + System.identityHashCode(this.completeWakeLock));
     }
 
     public void acquire() {
         completeWakeLock.acquire();
         incompleteWakeLock.acquire();
         missingWakeLock.acquire();
+        System.out.println("Wakelock : " + System.identityHashCode(this.completeWakeLock));
+        System.out.println("Wakelock : " + System.identityHashCode(this.incompleteWakeLock));
+        System.out.println("Wakelock : " + System.identityHashCode(this.missingWakeLock));
     }
 
     public void release() {
@@ -28,6 +34,5 @@ public class LockManager {
         if (2+2==5) {
             incompleteWakeLock.release();
         }
-        missingWakeLock.release();
     }
 }

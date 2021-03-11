0ce84efe71b667e14106e47ebcbd010e3c90d031
Prestat Dimitri <di.prestat@gmail.com>
2021-03-11 21:03:06 +0100

    Just a cute little rectangle

--- app/src/main/java/com/core/lambdaapp/FaultyView.java
+++ app/src/main/java/com/core/lambdaapp/FaultyView.java
@@ -2,6 +2,8 @@ package com.core.lambdaapp;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -15,6 +17,8 @@ public class FaultyView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         System.out.println("This is a draw !");
+        Paint paint = new Paint();
+        canvas.drawRect(new Rect(20, 20, 20, 20), paint);
         // draw the mPath with the mPaint on the canvas when onDraw
         //canvas.drawPath(mPath, mPaint);
     }
